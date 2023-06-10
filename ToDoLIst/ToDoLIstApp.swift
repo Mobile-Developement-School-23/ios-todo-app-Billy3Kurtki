d7bf6909f71ba0e5322050e3ed7813a90fc45163
Kirill <kirill.kazakov26@gmail.com>
2023-06-10 13:08:36 +0300

    Initial Commit

---
+++ ToDoLIst/ToDoLIstApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoLIstApp.swift
+//  ToDoLIst
+//
+//  Created by Кирилл Казаков on 10.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct ToDoLIstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
