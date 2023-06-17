7e3da9a5a283fd43f904781f210fd0544d804ca7
Kirill <kirill.kazakov26@gmail.com>
2023-06-17 03:04:44 +0300

    added parse(csv), csv: String on TodoItem, saveFile(csv) and getAllFromCsv() methods on FileCache

--- ToDoLIst/Models/FileCache.swift
+++ ToDoLIst/Models/FileCache.swift
@@ -16,7 +16,7 @@ class FileCache {
         }
     }
     
-    func saveJsonFile(fileName: String) {
+    func saveAllJsonFile(fileName: String) {
         let jsonDict = toDoList.map { $0.json }
         do {
             let jsonData = try JSONSerialization.data(withJSONObject: jsonDict)
@@ -73,12 +73,58 @@ class FileCache {
         self.toDoList = toDoList
     }
     
+    
+    static let delimiter = TodoItem.delimiter
+    
     func saveAllCsvFile(fileName: String) {
-        let csvDict = toDoList.map { $0.csv }
-        
+        var csvString = ""
+        for i in toDoList {
+            csvString.append("\(i.csv)\n")
+        }
+        do {
+            let documentDirectory = try FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
+            let fileUrl = documentDirectory.appendingPathComponent("\(fileName)").appendingPathExtension("csv")
+            if FileManager.default.fileExists(atPath: fileUrl.path) {
+                print("Файл существует")
+                try csvString.write(to: fileUrl, atomically: true, encoding: String.Encoding.utf8)
+                
+            } else {
+                print("Файл не существует")
+                try csvString.write(to: fileUrl, atomically: true, encoding: String.Encoding.utf8)
+            }
+        } catch {
+            print("Ошибка создания CSV файла")
+            return
+        }
     }
     
     func getAllFromCsv(fileName: String) {
+        let path = Bundle.main.path(forResource: "\(fileName)", ofType: "csv")
+        var data = ""
         
+        if let csv = path {
+            do {
+                data = try String(contentsOfFile: csv)
+            } catch {
+                print("Ошибка получения данных из CSV файла: \(error.localizedDescription)")
+                return
+            }
+        }
+        
+        var rows = data.components(separatedBy: "\n")
+        
+        let columnCount = rows.first?.components(separatedBy: "delimiter").count
+        rows.removeFirst()
+        
+        var toDoList: [TodoItem] = []
+        for row in rows {
+            if row.components(separatedBy: FileCache.delimiter).count == columnCount {
+                if let item = TodoItem.parse(csv: row) {
+                    toDoList.append(item)
+                }
+            }
+        }
+        
+        self.toDoList = toDoList
     }
 }
--- ToDoLIst/Models/TodoItem.swift
+++ ToDoLIst/Models/TodoItem.swift
@@ -22,25 +22,25 @@ extension TodoItem {
             if let id = json["id"] {
                 guard let id = id as? String else { return nil }
             }
-            if let text = json["text"] {
-                guard let text = text as? String else { return nil }
-            }
-            if let importance = json["importance"] {
-                guard let importance = importance as? Importance.RawValue else { return nil }
-            }
+            
+            guard let text = json["text"] else { return nil }
+            guard let text = text as? String else { return nil }
+            
             if let deadline = json["deadline"] {
                 guard let deadline = deadline as? Int else { return nil }
             }
-            if let isDone = json["isDone"] {
-                guard let isDone = isDone as? Bool else { return nil }
-            }
-            if let createAt = json["createAt"] {
-                guard let createAt = createAt as? Int else { return nil }
-            }
+            
+            guard let isDone = json["isDone"] else { return nil }
+            guard let isDone = isDone as? Bool else { return nil }
+
+            guard let createAt = json["createAt"] else { return nil }
+            guard let createAt = createAt as? Int else { return nil }
+            
             if let dateEdit = json["dateEdit"] {
                 guard let dateEdit = dateEdit as? Int else { return nil }
             }
-            var item: TodoItem = TodoItem(
+            
+            let item: TodoItem = TodoItem(
                 id: json["id"] as? String ?? UUID().uuidString,
                 text: json["text"] as? String ?? "",
                 importance: Importance(rawValue: (json["importance"] as? Importance.RawValue ?? Importance.ordinary.rawValue)) ?? .ordinary,
@@ -82,13 +82,60 @@ extension TodoItem {
     static let delimiter = ";"
     
     static func parse(csv: String) -> TodoItem? {
+        let row = csv.components(separatedBy: delimiter)
+        var importanceTemp = Importance.ordinary
+        if row[1].isEmpty {
+            return nil
+        }
+        if !row[2].isEmpty {
+            guard let importance = Importance(rawValue: row[2]) else { return nil }
+            importanceTemp = importance
+        }
         
-        return nil
+        if row[4].isEmpty {
+            return nil
+        }
+        guard let isDone = Bool(row[4]) else { return nil }
+        
+        if row[5].isEmpty {
+            return nil
+        }
+        guard let createAt = Int(row[5]) else { return nil }
+        
+        let item: TodoItem = TodoItem(
+            id: !row[0].isEmpty ? row[0] : UUID().uuidString,
+            text: row[1],
+            importance: importanceTemp,
+            deadline: Int(row[3])?.date ?? nil,
+            isDone: isDone,
+            createAt: Int(row[5])?.date ?? Date(),
+            dateEdit: createAt.date
+        )
+        
+        return item
     }
     
     var csv: String {
         var toDoItem: String = ""
-        
+        if let id = id {
+            toDoItem.append("\(id)")
+        }
+        toDoItem.append(";")
+        toDoItem.append("\(text);")
+        if importance != Importance.ordinary  {
+            toDoItem.append("\(importance)")
+        }
+        toDoItem.append(";")
+        if let deadline = deadline {
+            toDoItem.append("\(deadline)")
+        }
+        toDoItem.append(";")
+        toDoItem.append("\(isDone);")
+        toDoItem.append("\(createAt);")
+        if let dateEdit = dateEdit {
+            toDoItem.append("\(dateEdit)")
+        }
+        toDoItem.append(";")
         
         return toDoItem
     }
