458c88c64c44058504324e9c85a1cf3317d08c67
Kirill <kirill.kazakov26@gmail.com>
2023-06-27 15:12:26 +0300

    added secondVC with second display

--- ToDoLIst/SecondViewController.swift
+++ ToDoLIst/SecondViewController.swift
@@ -7,15 +7,280 @@
 
 import UIKit
 
-class SecondViewContoller: UIViewController {
+class SecondViewContoller: UIViewController, UITextViewDelegate {
     
-    let button = UIButton()
+    let titleLabel = UILabel()
+    let buttonDismiss = UIButton()
+    let buttonSave = UIButton()
+    let textView = UITextView()
+    let tableView = UITableView()
+    let deleteButton = UIButton()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        button.translatesAutoresizingMaskIntoConstraints = false
-        self.view.addSubview(button)
+        self.view.backgroundColor = UIColor.gray
         
-        button.setTitle("ТЕСТ", for: .normal)
+        buttonDismiss.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(buttonDismiss)
+        let tapRecognizer = UITapGestureRecognizer(target: self, action: #selector(self.handleTapGesture))
+        
+        buttonDismiss.addGestureRecognizer(tapRecognizer)
+        buttonDismiss.setTitle("Отменить", for: .normal)
+        buttonDismiss.setTitleColor(UIColor.blue, for: .normal)
+        
+        NSLayoutConstraint.activate([
+            buttonDismiss.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
+            buttonDismiss.topAnchor.constraint(equalTo: view.topAnchor, constant: 20),
+            buttonDismiss.widthAnchor.constraint(equalToConstant: 100),
+            buttonDismiss.heightAnchor.constraint(equalToConstant: 30)
+        ])
+        
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.text = "Дело"
+        titleLabel.font = .boldSystemFont(ofSize: 20)
+        self.view.addSubview(titleLabel)
+        
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 20),
+            titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            titleLabel.widthAnchor.constraint(equalToConstant: 60),
+            titleLabel.heightAnchor.constraint(equalToConstant: 30)
+        ])
+        
+        buttonSave.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(buttonSave)
+        
+        buttonSave.setTitle("Сохранить", for: .normal)
+        buttonSave.setTitleColor(UIColor.blue, for: .normal)
+        
+        NSLayoutConstraint.activate([
+            buttonSave.leadingAnchor.constraint(equalTo: titleLabel.trailingAnchor, constant: 10),
+            buttonSave.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 30),
+            buttonSave.topAnchor.constraint(equalTo: view.topAnchor, constant: 20),
+            buttonSave.widthAnchor.constraint(equalToConstant: 60),
+            buttonSave.heightAnchor.constraint(equalToConstant: 30)
+        ])
+        
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(textView)
+        textView.delegate = self
+        textView.text = "Что надо сделать?"
+        textView.font = .systemFont(ofSize: 20)
+        textView.textColor = UIColor.lightGray
+        let range = NSMakeRange(textView.text.count - 1, 0)
+        textView.scrollRangeToVisible(range)
+        textView.textContainer.lineFragmentPadding = 20
+//        textView.textContainer.lineBreakMode = .byTruncatingTail
+        textView.layer.cornerRadius = 20
+        
+        NSLayoutConstraint.activate([
+            textView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            textView.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 20),
+            textView.widthAnchor.constraint(equalToConstant: 360),
+            textView.heightAnchor.constraint(equalToConstant: 120)
+        ])
+        
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.layer.cornerRadius = 20
+        tableView.register(ImportanceSegControlCell.self, forCellReuseIdentifier: "importance")
+        tableView.register(DateSwitcherCell.self, forCellReuseIdentifier: "dateSwitch")
+        tableView.register(CalendarCell.self, forCellReuseIdentifier: "calendar")
+        
+        NSLayoutConstraint.activate([
+            tableView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            tableView.topAnchor.constraint(equalTo: textView.bottomAnchor, constant: 20),
+            tableView.widthAnchor.constraint(equalToConstant: 360),
+            tableView.heightAnchor.constraint(equalToConstant: 500)
+        ])
+        
+        deleteButton.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(deleteButton)
+        
+        deleteButton.setTitle("Удалить", for: .normal)
+        deleteButton.setTitleColor(UIColor.red, for: .normal)
+        deleteButton.backgroundColor = UIColor.white
+        deleteButton.layer.cornerRadius = 16
+        
+        NSLayoutConstraint.activate([
+            deleteButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            deleteButton.topAnchor.constraint(equalTo: tableView.bottomAnchor, constant: 20),
+            deleteButton.widthAnchor.constraint(equalToConstant: 360),
+            deleteButton.heightAnchor.constraint(equalToConstant: 50)
+        ])
+        
+    }
+    
+    @objc func handleTapGesture(sender: UITapGestureRecognizer) {
+        dismiss(animated: true)
+    }
+    
+    func textViewDidBeginEditing(_ textView: UITextView) {
+
+        if textView.textColor == UIColor.lightGray {
+            textView.text = ""
+            textView.textColor = UIColor.black
+        }
+    }
+    
+    func textViewDidEndEditing(_ textView: UITextView) {
+
+        if textView.text == "" {
+            textView.text = "Что надо сделать?"
+            textView.textColor = UIColor.lightGray
+        }
+    }
+}
+
+extension SecondViewContoller: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 67
+    }
+}
+
+extension SecondViewContoller: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if indexPath.row == 0 {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "importance", for: indexPath) as! ImportanceSegControlCell
+            return cell
+        }
+        else if indexPath.row == 1 {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "dateSwitch", for: indexPath) as! DateSwitcherCell
+            return cell
+        }
+        else {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "calendar", for: indexPath) as! CalendarCell
+            return cell
+        }
+    }
+}
+
+
+class ImportanceSegControlCell: UITableViewCell {
+
+    let taskLabel = UILabel()
+    let importanceSwitcher = UISegmentedControl()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        taskLabel.translatesAutoresizingMaskIntoConstraints = false
+        contentView.addSubview(taskLabel)
+        
+        taskLabel.text = "Важность"
+        taskLabel.textColor = UIColor.black
+        taskLabel.font = .systemFont(ofSize: 17)
+        
+        NSLayoutConstraint.activate([
+            taskLabel.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            taskLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 20),
+            taskLabel.heightAnchor.constraint(equalToConstant: 40),
+            taskLabel.widthAnchor.constraint(equalToConstant: 100)
+        ])
+        
+        importanceSwitcher.translatesAutoresizingMaskIntoConstraints = false
+        contentView.addSubview(importanceSwitcher)
+        
+        importanceSwitcher.insertSegment(with: UIImage(systemName: "arrow.down"), at: 0, animated: true)
+        importanceSwitcher.insertSegment(withTitle: "нет", at: 1, animated: true)
+        importanceSwitcher.insertSegment(withTitle: "!!", at: 2, animated: true)
+        importanceSwitcher.selectedSegmentIndex = 1
+        importanceSwitcher.backgroundColor = UIColor.lightGray
+        importanceSwitcher.selectedSegmentTintColor = UIColor.white
+        
+        NSLayoutConstraint.activate([
+            importanceSwitcher.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            importanceSwitcher.leadingAnchor.constraint(equalTo: taskLabel.trailingAnchor, constant: 75),
+            importanceSwitcher.heightAnchor.constraint(equalToConstant: 36),
+            importanceSwitcher.widthAnchor.constraint(equalToConstant: 150)
+        ])
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+class DateSwitcherCell: UITableViewCell {
+    let switcher = UISwitch()
+    let label = UILabel()
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        contentView.addSubview(label)
+        label.text = "Сделать до"
+        label.textColor = UIColor.black
+        label.font = .systemFont(ofSize: 17)
+        
+        NSLayoutConstraint.activate([
+            label.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            label.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 20),
+            label.heightAnchor.constraint(equalToConstant: 40),
+            label.widthAnchor.constraint(equalToConstant: 100)
+        ])
+        
+        switcher.translatesAutoresizingMaskIntoConstraints = false
+        contentView.addSubview(switcher)
+        switcher.isOn = false
+        switcher.onTintColor = UIColor.green
+        
+        NSLayoutConstraint.activate([
+            switcher.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            switcher.leadingAnchor.constraint(equalTo: label.trailingAnchor, constant: 175),
+            switcher.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: 0),
+//            switcher.heightAnchor.constraint(equalToConstant: 40)
+        ])
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+class CalendarCell: UITableViewCell {
+    let calendarView = UICalendarView()
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        calendarView.translatesAutoresizingMaskIntoConstraints = false
+        contentView.addSubview(calendarView)
+        
+        let gregorianCalendar = Calendar(identifier: .gregorian)
+        calendarView.calendar = gregorianCalendar
+        calendarView.tintColor = .white
+        calendarView.availableDateRange = DateInterval(start: .now, end: .distantFuture)
+        
+        calendarView.fontDesign = .rounded
+        let selection = UICalendarSelectionSingleDate(delegate: self)
+        calendarView.selectionBehavior = selection
+        calendarView.tintColor = UIColor.black
+        calendarView.backgroundColor = UIColor.white
+//        NSLayoutConstraint.activate([
+//            calendarView.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+//            calendarView.centerXAnchor.constraint(equalTo: contentView.ce),
+//            calendarView.leadingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: 175),
+//            calendarView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: 0),
+//          switcher.heightAnchor.constraint(equalToConstant: 40)
+//        ])
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension CalendarCell: UICalendarSelectionSingleDateDelegate {
+    func dateSelection(_ selection: UICalendarSelectionSingleDate, didSelectDate dateComponents: DateComponents?) {
+        print(dateComponents)
+    }
+    func dateSelection(_ selection: UICalendarSelectionSingleDate, canSelectDate dateComponents: DateComponents?) -> Bool {
+        true
     }
 }
--- TodoList/ViewController.swift
+++ TodoList/ViewController.swift
@@ -9,9 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+   
+    var todoItem1 = TodoItem(
+        id: "1",
+        text: "Помыть машину",
+        importance: Importance.important,
+        deadline: Date(),
+        isDone: false,
+        createAt: Date(),
+        dateEdit: Date()
+    )
+    
+    var todoItem2 = TodoItem(
+        id: "2",
+        text: "Убраться в доме",
+        importance: Importance.unimportant,
+        deadline: Date(),
+        isDone: true,
+        createAt: Date(),
+        dateEdit: Date()
+    )
+    var flag = false
+    var collection: [TodoItem] = []
+    
     let titleLabel = UILabel()
     let doneTasksLabel = UILabel()
     let showDoneTasksButton = UIButton()
+    let heightForRow: CGFloat = 67
     
     let tableView: UITableView = {
         let table = UITableView()
@@ -38,8 +62,15 @@ class ViewController: UIViewController {
     
     let data = ["Work1", "Work2", "Work3", "Work4", "Work5", "Work6", "Work7", "Work8", "Work9", "Work10", "Work11"]
     let identifier = "cell"
+    
+    var filecache = FileCache()
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        filecache.addItem(todoItem1)
+        filecache.addItem(todoItem2)
+        filecache.saveAllJsonFile(fileName: "jsonfile")
+        filecache.getAllFromJson(fileName: "jsonfile")
         
         titleLabel.translatesAutoresizingMaskIntoConstraints = false
         self.view.addSubview(titleLabel)
@@ -52,8 +83,8 @@ class ViewController: UIViewController {
         
         doneTasksLabel.translatesAutoresizingMaskIntoConstraints = false
         self.view.addSubview(doneTasksLabel)
-        
-        doneTasksLabel.text = "Выполнено - 0"
+        let countDone = filecache.toDoList.filter({$0.isDone == true}).count
+        doneTasksLabel.text = "Выполнено - \(countDone)"
         doneTasksLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 34).isActive = true
         doneTasksLabel.topAnchor.constraint(equalTo: titleLabel.topAnchor, constant: 55).isActive = true
         doneTasksLabel.widthAnchor.constraint(equalToConstant: 120).isActive = true
@@ -61,8 +92,10 @@ class ViewController: UIViewController {
         
         showDoneTasksButton.translatesAutoresizingMaskIntoConstraints = false
         self.view.addSubview(showDoneTasksButton)
-        
+        let tapRecognizer = UITapGestureRecognizer(target: self, action: #selector(self.handleShowDoneTasks))
         showDoneTasksButton.setTitle("Показать", for: .normal)
+        
+        showDoneTasksButton.addGestureRecognizer(tapRecognizer)
         showDoneTasksButton.setTitleColor(UIColor.blue, for: .normal)
         showDoneTasksButton.leadingAnchor.constraint(equalTo: doneTasksLabel.trailingAnchor, constant: 110).isActive = true
         showDoneTasksButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -15).isActive = true
@@ -70,15 +103,17 @@ class ViewController: UIViewController {
         
         self.view.addSubview(tableView)
         
-        tableView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 15.0).isActive = true
+        tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 15.0).isActive = true
         tableView.topAnchor.constraint(equalTo: doneTasksLabel.bottomAnchor, constant: 10).isActive = true
-        tableView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -15.0).isActive = true
-        tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0.0).isActive = true
+        tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -15.0).isActive = true
+        tableView.heightAnchor.constraint(equalToConstant: heightForRow * 3).isActive = true
+//        tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0.0).isActive = true
         tableView.layer.cornerRadius = 20
         tableView.delegate = self
         tableView.dataSource = self
         tableView.separatorInset = .init(top: 20, left: 65, bottom: 20, right: 0)
         tableView.register(CustomCell.self, forCellReuseIdentifier: identifier)
+        
         
         
         self.view.addSubview(addButton)
@@ -87,7 +122,7 @@ class ViewController: UIViewController {
         addButton.widthAnchor.constraint(equalToConstant: 52).isActive = true
         addButton.heightAnchor.constraint(equalToConstant: 52).isActive = true
         addButton.centerXAnchor.constraint(equalTo: self.tableView.centerXAnchor).isActive = true
-//        addButton.addTarget(self, action: #selector(self.showCreateView(btnAction)), for:.touchUpInside)
+        addButton.addTarget(self, action: #selector(self.showCreateView(sender: )), for:.touchUpInside)
         
         self.view.backgroundColor = UIColor.gray
     }
@@ -98,25 +133,83 @@ class ViewController: UIViewController {
     }
     
     func tappedCell() {}
+    
+    @objc func handleShowDoneTasks(sender: UITapGestureRecognizer) {
+        flag.toggle()
+        if !flag {
+            showDoneTasksButton.setTitle("Показать", for: .normal)
+        }
+        else {
+            showDoneTasksButton.setTitle("Скрыть", for: .normal)
+        }
+        tableView.reloadData()
+    }
+    
 }
 
 extension ViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 67
+        return heightForRow
     }
 }
 
 extension ViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return data.count
+        if !flag {
+            return filecache.toDoList.filter({$0.isDone == false}).count
+        }
+        else {
+            return filecache.toDoList.count
+        }
+//        if !flag {
+//            let listCount = filecache.toDoList.filter({$0.isDone == false}).count
+//            tableView.heightAnchor.constraint(equalToConstant: heightForRow * CGFloat(listCount)).isActive = true
+//            tableView.updateConstraints()
+//            return listCount
+//        }
+//        else {
+//            let listCount = filecache.toDoList.count
+//            tableView.heightAnchor.constraint(equalToConstant: heightForRow * CGFloat(listCount)).isActive = true
+//            tableView.updateConstraints()
+//            return listCount
+//        }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! CustomCell
-        cell.circleImage.image = UIImage(systemName: "circle")
-        cell.circleImage.tintColor = UIColor.gray
-        cell.taskLabel.text = data[indexPath.row]
-        cell.dateLabel.text = "test"
+        if filecache.toDoList[indexPath.row].isDone {
+            cell.circleImage.image = UIImage(systemName: "checkmark.circle.fill")
+            cell.circleImage.backgroundColor = UIColor.white
+            cell.circleImage.tintColor = UIColor.green
+            let attributedText = NSAttributedString(
+                string: filecache.toDoList[indexPath.row].text,
+                attributes: [.strikethroughStyle: NSUnderlineStyle.single.rawValue]
+            )
+            cell.taskLabel.attributedText = attributedText
+            cell.taskLabel.topAnchor.constraint(equalTo: cell.contentView.topAnchor, constant: 18).isActive = true
+            cell.dateLabel.isHidden = true
+            cell.calendarImage.isHidden = true
+        }
+        else {
+            if filecache.toDoList[indexPath.row].importance == Importance.important {
+                cell.circleImage.image = UIImage(systemName: "circle")
+                cell.circleImage.tintColor = UIColor.red
+            }
+            else {
+                cell.circleImage.image = UIImage(systemName: "circle")
+                cell.circleImage.tintColor = UIColor.gray
+            }
+            cell.taskLabel.topAnchor.constraint(equalTo: cell.contentView.topAnchor, constant: 10).isActive = true
+            cell.taskLabel.text = filecache.toDoList[indexPath.row].text
+        }
+        
+        cell.taskLabel.text = filecache.toDoList[indexPath.row].text
+        if let deadline = filecache.toDoList[indexPath.row].deadline {
+            let dateStringFormatter = DateFormatter()
+            dateStringFormatter.dateFormat = "dd MMMM"
+            dateStringFormatter.locale = Locale(identifier: "ru_RU")
+            cell.dateLabel.text = dateStringFormatter.string(for: deadline)
+        }
         cell.button.setImage(UIImage(systemName: "chevron.right"), for: .normal)
         cell.button.tintColor = UIColor.gray
         return cell
@@ -141,7 +234,7 @@ extension ViewController: UITableViewDataSource {
         ])
     }
     
-    func showCreateView() {
+    @objc func showCreateView(sender: Any) {
         let secondVC = SecondViewContoller()
         if let sheet = secondVC.sheetPresentationController {
             sheet.detents = [.large()]
@@ -159,6 +252,7 @@ class CustomCell: UITableViewCell {
     let dateLabel = UILabel()
     let button = UIButton()
     var circleImage = UIImageView()
+    var calendarImage = UIImageView()
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
@@ -180,15 +274,27 @@ class CustomCell: UITableViewCell {
         
         NSLayoutConstraint.activate([
             taskLabel.leadingAnchor.constraint(equalTo: circleImage.trailingAnchor, constant: 15),
-            taskLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 10),
+//            taskLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 10),
             taskLabel.heightAnchor.constraint(equalToConstant: 30)
+        ])
+        
+        calendarImage.translatesAutoresizingMaskIntoConstraints = false
+        contentView.addSubview(calendarImage)
+        
+        calendarImage.image = UIImage(systemName: "calendar")
+        calendarImage.tintColor = UIColor.lightGray
+        
+        NSLayoutConstraint.activate([
+            calendarImage.leadingAnchor.constraint(equalTo: circleImage.trailingAnchor, constant: 15),
+            calendarImage.topAnchor.constraint(equalTo: taskLabel.topAnchor, constant: 28),
+//            calendarImage.heightAnchor.constraint(equalToConstant: 25)
         ])
         
         dateLabel.translatesAutoresizingMaskIntoConstraints = false
         dateLabel.textColor = UIColor.gray
         contentView.addSubview(dateLabel)
         NSLayoutConstraint.activate([
-            dateLabel.leadingAnchor.constraint(equalTo: circleImage.trailingAnchor, constant: 15),
+            dateLabel.leadingAnchor.constraint(equalTo: calendarImage.trailingAnchor, constant: 5),
             dateLabel.topAnchor.constraint(equalTo: taskLabel.topAnchor, constant: 25),
             dateLabel.heightAnchor.constraint(equalToConstant: 25)
         ])
@@ -212,5 +318,4 @@ class CustomCell: UITableViewCell {
         fatalError("init(coder:) has not been implemented")
     }
 }
-
 
